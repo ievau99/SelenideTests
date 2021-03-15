2e383d913ae0d59ed4ebf3fd4e0b70bf5e26345d
Ieva Blinstrubienė <ievau99@gmail.com>
2021-03-15 11:26:02 +0200

    1

---
+++ src/test/java/com/example/demo/MyTest.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @Test
+    public void myTest() {
+
+
+    }
+
+}
